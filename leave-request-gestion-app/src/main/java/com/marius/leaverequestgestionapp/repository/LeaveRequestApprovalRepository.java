d6e60321eb9922640215420c6901b512d3a27c71
Bogdan Fatu <ionutobgdan11@gmail.com>
2021-01-09 22:49:07 +0200

    fixed leave request approval repo

--- leave-request-gestion-app/src/main/java/com/marius/leaverequestgestionapp/repository/LeaveRequestApprovalRepository.java
+++ leave-request-gestion-app/src/main/java/com/marius/leaverequestgestionapp/repository/LeaveRequestApprovalRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LeaveRequestApprovalRepository extends JpaRepository<LeaveRequestApproval, Long> {
 
-    LeaveRequestApproval get(long leaveRequestId);
+    LeaveRequestApproval findAllByManager(long managerId);
 
 }
